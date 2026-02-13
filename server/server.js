22e8b91e11ed6a743e6957786ee0f458f177fc26
janhavi-sharma <janhavi.p.sharma@gmail.com>
2026-02-13 21:34:05 +0530

    Fix production server setup

--- server/server.js
+++ server/server.js
@@ -29,7 +29,8 @@ app.use("/api/contact", contactRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("/:path(*)", (req, res) => {
+  // Catch-all route
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
   });
 }
